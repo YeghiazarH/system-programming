bdf14ac00c6aada6cfebd0891648a5197a04beca
YeghiazarH <94860680+YeghiazarH@users.noreply.github.com>
2025-12-01 23:18:11 +0400

    Replace std::cout with write functions for output

--- IPC-1/main.cpp
+++ IPC-1/main.cpp
@@ -3,7 +3,17 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <ucontext.h>
+#include <string.h>
 
+void write_str(const char* s) {
+    write(1, s, strlen(s));
+}
+
+void write_num(long v) {
+    char buf[64];
+    int len = snprintf(buf, sizeof(buf), "%ld", v);
+    write(1, buf, len);
+}
 
 void handler(int sig, siginfo_t *si, void *context) {
     (void)sig;
@@ -12,7 +22,7 @@ void handler(int sig, siginfo_t *si, void *context) {
 
     const char* username = "(unknown)";
     struct passwd* pw = getpwuid(uid);
-    if (pw) username = pw->pw_name;
+    if(pw) username = pw->pw_name;
 
     ucontext_t* uc = (ucontext_t*)context;
     unsigned long ip = 0, ax = 0, bx = 0;
@@ -26,32 +36,40 @@ void handler(int sig, siginfo_t *si, void *context) {
     ax = uc->uc_mcontext.gregs[REG_EAX];
     bx = uc->uc_mcontext.gregs[REG_EBX];
 #endif
-    std::cout << "Received SIGUSR1 from PID "
-              << pid << " executed by UID "
-              << uid << " (" << username << ")" << std::endl;
 
-    std::cout << "State of context: IP = " << ip
-              << " AX = " << ax
-              << " BX = " << bx
-              << std::endl;
+    write_str("Received SIGUSR1 from PID ");
+    write_num(pid);
+    write_str(" executed by UID ");
+    write_num(uid);
+    write_str(" (");
+    write_str(username);
+    write_str(")\n");
+
+    write_str("State of context: IP = ");
+    write_num(ip);
+    write_str(" AX = ");
+    write_num(ax);
+    write_str(" BX = ");
+    write_num(bx);
+    write_str("\n");
 }
 
 int main() {
-    //usage// kill -SIGUSR1 PID
-    std::cout<<"PID: "<<getpid()<<std::endl;
+    write_str("PID: ");
+    write_num(getpid());
+    write_str("\n");
 
     struct sigaction sa = {};
     sa.sa_sigaction = handler;
     sa.sa_flags = SA_SIGINFO;
     sigemptyset(&sa.sa_mask);
 
-    if (sigaction(SIGUSR1, &sa, nullptr) == -1) {
+    if(sigaction(SIGUSR1, &sa, nullptr) == -1) {
         perror("sigaction");
         return 1;
     }
 
-    while (1) sleep(10);
+    while(1) sleep(10);
 
     return 0;
 }
-
